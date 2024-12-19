638df14b682890fe3fba89c82def1fd7d0f8304c
Aiden <aidenhosseini@outlook.de>
2024-12-19 17:51:34 +0100

    Create setup-xubuntu.sh

---
+++ setup-xubuntu.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Update package list and install necessary packages
+sudo apt update
+sudo apt install xfce4 xfce4-goodies novnc python3-websockify python3-numpy tightvncserver htop nano neofetch -y
+
+# Generate SSL certificate for noVNC
+openssl req -x509 -nodes -newkey rsa:3072 -keyout novnc.pem -out novnc.pem -days 3650
+
+# Start VNC server (root user)
+USER=root vncserver
+
+# Kill any existing VNC sessions
+vncserver -kill :1
+
+# Backup the current xstartup file and create a new one
+mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
+
+# Create a new xstartup file to launch Xfce desktop environment
+echo -e '#!/bin/sh\nxrdb $HOME/.Xresources\nstartxfce4 &' > ~/.vnc/xstartup
+
+# Start VNC server again
+USER=root vncserver
+
+# Start websockify to enable noVNC in the browser
+websockify -D --web=/usr/share/novnc/ --cert=/home/ubuntu/novnc.pem 6080 localhost:5901
